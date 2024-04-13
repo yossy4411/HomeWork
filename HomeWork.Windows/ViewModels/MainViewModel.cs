c27102abfc82ab6224b5b1d7883ab57ff6ca72d1
yossy4411 <yaidonliving@gmail.com>
2024-04-13 21:30:48 +0900

    Wpf追加

---
+++ HomeWork.Windows/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HomeWork.Windows.Views;
+using Wpf.Ui.Controls;
+
+namespace HomeWork.Windows.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<NavigationViewItem> NavigationViewItems { get; set; } = [
+                new("ホーム", SymbolRegular.Home24, typeof(HomePage)),
+                new("カレンダー", SymbolRegular.Calendar24, typeof(CalendarPage))
+            ];
+    }
+}
